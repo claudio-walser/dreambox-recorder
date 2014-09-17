177bf353ef9df97a435495ae38190ab391b28ba1
Claudio Walser <claudio@claudio.dev>
2014-09-17 23:55:49 +0200

    Thinking loud cause i still dont have a nice solution for acl

--- Dto/Broadcast.php
+++ Dto/Broadcast.php
@@ -2,7 +2,8 @@
 /**
  * Todo: generate in orm maybe? Implement ACL for json clients here maybe?
  * Same controller returns different dto's containg more or less data, depending on the client
- * acl could be defined in xml where database is described... Just thinking and try to dont forget soon...
+ * acl could be defined in xml where database is described... 
+ * Just thinking and try to dont forget soon...
  */
 namespace DreamboxRecorder\Dto;
 

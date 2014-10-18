78789c1c5790567c18727f8544500c45a87d4043
Claudio Walser <claudio@claudio.dev>
2014-10-18 14:36:28 +0200

    Simplified dto objects

--- Dto/AbstractDto.php
+++ Dto/AbstractDto.php
@@ -1,8 +1,44 @@
 <?php
 namespace DreamboxRecorder\Dto;
 
-class AbstractDto implements \JsonSerializable {
+class AbstractDto extends \StdClass implements \JsonSerializable {
 	
+	public function __call($methodName, $arguments) {
+		// only get and set methods, i dont care
+		$keyName = lcfirst(substr($methodName, 3, strlen($methodName)));
+		$method = strtolower(substr($methodName, 0, 3));
+		
+		switch ($method) {
+			case 'set':
+				if (is_array($arguments) && !empty($arguments[0])) {
+					$value = is_numeric($arguments[0]) ? (int) $arguments[0] : (string) $arguments[0];
+					$this->{$keyName} = $value;
+				}
+				return true;
+				break; //know i dont need it, just to dont violate switch/case
+
+			case 'get':
+				if (!isset($this->{$keyName})) {
+					throw new \Exception('You are trying to do something nasty - ' . $keyName . ' isnt set yet.');
+				}
+				return $this->{$keyName};
+				break; //same
+
+			default:
+
+				throw new \Exception('A DataTransferObject usually only have get/set Methods or public properties, you tried to call ' . $methodName . ' method which does nothing.');
+				break; // same
+		}
+
+	}
+
+	public function __set($key, $value) {
+		$this->$key = $value;
+	}
+
+	public function __get($key) {
+		return isset($this->$key) ? $this->$key : null;
+	}
 
 	public function jsonSerialize() {
 		return  $this->__toArray();
@@ -12,7 +48,6 @@ class AbstractDto implements \JsonSerializable {
 		$props = array();
 
 		foreach ($this as $key => $property) {
-			$key = substr($key, 1);
 			$props[$key] = $property;
 		}
 		
--- Dto/Channel.php
+++ Dto/Channel.php
@@ -8,27 +8,9 @@
 namespace DreamboxRecorder\Dto;
 
 class Channel extends AbstractDto {
-	
-	protected $_name = '';
-	protected $_reference = '';
 
-
-
-	public function setName($name) {
-		$this->_name = (string) $name;
-	}
-
-	public function getName() {
-		return $this->_name;
-	}
-
-	public function setReference($reference) {
-		$this->_reference = (string) $reference;
-	}
-
-	public function getReference() {
-		return $this->_reference;
-	}
+	protected $name = '';
+	protected $reference = '';
 
 }
 ?>
